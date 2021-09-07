6102b9ad6881abcfebd78072f0df29387bda5d8b
giu0213@naver.com <giu0213@naver.com>
2021-09-07 14:50:16 +0900

    [ADD] routes & controller

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const productRoutes = require("./routes");
+
+app.use(express.json());
+
+app.use("/api/product", productRoutes);
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
