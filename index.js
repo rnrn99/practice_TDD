154dc74221fa6de4c410ceb2e2acac85e8bad65b
giu0213@naver.com <giu0213@naver.com>
2021-09-08 18:21:03 +0900

    [ADD] mongoDB setting & product model

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 5000;
-
 const productRoutes = require("./routes");
+
+mongoose
+  .connect(
+    "mongodb+srv://practice_tdd:passwordtdd@practice.lmq5s.mongodb.net/practiceTDD?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    },
+  )
+  .then(() => console.log("mongoDB connected!"))
+  .catch((err) => console.log(err));
 
 app.use(express.json());
 
---
+++ model/Product.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: Number,
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
