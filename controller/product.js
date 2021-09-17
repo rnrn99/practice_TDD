2f08a70b21f2335791a48135ed75140b8a90249c
giu0213@naver.com <giu0213@naver.com>
2021-09-17 14:44:26 +0900

    [ADD] Update - Unit Test

--- controller/product.js
+++ controller/product.js
@@ -28,3 +28,19 @@ exports.getProductById = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.updateProduct = async (req, res, next) => {
+  try {
+    const updatedProduct = await productModel.findByIdAndUpdate(
+      req.params.productId,
+      req.body,
+      {
+        new: true,
+      },
+    );
+    if (updatedProduct) res.status(200).json(updatedProduct);
+    else res.status(404).send();
+  } catch (error) {
+    next(error);
+  }
+};
--- test/unit/product.test.js
+++ test/unit/product.test.js
@@ -7,9 +7,14 @@ const allProduct = require("../data/allProduct.json");
 productModel.create = jest.fn();
 productModel.find = jest.fn();
 productModel.findById = jest.fn();
+productModel.findByIdAndUpdate = jest.fn();
 
 let req, res, next;
 const productId = "613efebe4e6ce199056d5453";
+const updatedProduct = {
+  name: "updated name",
+  description: "updated description",
+};
 
 beforeEach(() => {
   req = httpMocks.createRequest();
@@ -114,6 +119,48 @@ describe("Product Controller GetById", () => {
     const rejectedPromise = Promise.reject(errorMsg);
     productModel.findById.mockReturnValue(rejectedPromise);
     await productController.getProductById(req, res, next);
+    expect(next).toHaveBeenCalledWith(errorMsg);
+  });
+});
+
+describe("Product Controller Update", () => {
+  test("should have an updateProduct function", () => {
+    expect(typeof productController.updateProduct).toBe("function");
+  });
+
+  test("should call productModel.findByIdAndUpdate", async () => {
+    req.params.productId = productId;
+    req.body = updatedProduct;
+    await productController.updateProduct(req, res, next);
+    expect(productModel.findByIdAndUpdate).toHaveBeenCalledWith(
+      productId,
+      updatedProduct,
+      { new: true },
+    );
+  });
+
+  test("should return json body and response code 200", async () => {
+    req.params.productId = productId;
+    req.body = updatedProduct;
+    productModel.findByIdAndUpdate.mockReturnValue(updatedProduct);
+    await productController.updateProduct(req, res, next);
+    expect(res.statusCode).toBe(200);
+    expect(res._getJSONData()).toStrictEqual(updatedProduct);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+
+  test("should return 404 when item doesn't exist", async () => {
+    productModel.findByIdAndUpdate.mockReturnValue(null);
+    await productController.updateProduct(req, res, next);
+    expect(res.statusCode).toBe(404);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+
+  test("should handle error", async () => {
+    const errorMsg = { message: "Failed to update product" };
+    const rejectedPromise = Promise.reject(errorMsg);
+    productModel.findByIdAndUpdate.mockReturnValue(rejectedPromise);
+    await productController.updateProduct(req, res, next);
     expect(next).toHaveBeenCalledWith(errorMsg);
   });
 });
